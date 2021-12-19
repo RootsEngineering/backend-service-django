2241c1007191816a6dbe0f8dff0d51394b39e54d
Wiktor <wikifed@gmail.com>
2021-12-19 16:25:37 +0100

    Added boolean filter to search for users with and without reports

--- mysite/directory/api/filtersets.py
+++ mysite/directory/api/filtersets.py
@@ -7,6 +7,7 @@ User = get_user_model()
 
 class UsersFilterSet(filters.FilterSet):
     strict = True
+    with_reports = BooleanFilter(field_name='reports_to', method='filter_with_reports')
 
     class Meta:
         model = User
@@ -14,3 +15,10 @@ class UsersFilterSet(filters.FilterSet):
             'email',
             'is_active'
         ]
+
+    def filter_with_reports(self, queryset, name, value):
+        if value:
+            queryset = queryset.filter(reports_to_id__isnull=False)
+            return queryset
+        queryset = queryset.filter(reports_to_id__isnull=True)
+        return queryset
