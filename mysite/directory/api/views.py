4f17f8a80c5803bf63d8cc16327f80dba7c1a896
Wiktor <wikifed@gmail.com>
2021-12-19 15:35:45 +0100

    add function to gather all reports

--- mysite/directory/api/views.py
+++ mysite/directory/api/views.py
@@ -25,6 +25,20 @@ class UsersViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(users, many=True)
         return Response(serializer.data)
 
+
+    def reports(first_user_numbers: User.pk):
+        reports_id_list = []
+        while first_user_numbers:
+            if type(first_user_numbers) != int:
+                users = User.objects.filter(reports_to_id__in=[num for num in first_user_numbers])
+                first_user_numbers = [u.pk for u in users]
+                reports_id_list.extend(first_user_numbers)
+            else:
+                users = User.objects.filter(reports_to_id__exact=first_user_numbers)
+                first_user_numbers = [u.pk for u in users]
+                reports_id_list.extend(first_user_numbers)
+        return reports_id_list
+    
     def get_queryset(self):
         company = self.request.user.company
         return User.objects.filter(company=company)
